bcb2c92b35b635d1ea380d07f05214673cf30368
tukloy <rheyvince.26@gmail.com>
2026-03-26 21:40:01 +0800

    Fix responsive in hero section when 4k resolution

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -2,15 +2,15 @@
 </script>
 <template>
     <section
-        class="bg-[#C7CEA5] h-[50vh] sm:h-[50vh]  md:h-[50vh] lg:h-[55vh] xl:h-[50vh] 2xl:h-[40vh] px-8 sm:px-8 md:px-8 lg:px-20 xl:px-50 2xl:px-80 relative overflow-hidden">
+        class="bg-[#C7CEA5] h-[50vh] sm:h-[50vh]  md:h-[50vh] lg:h-[55vh] xl:h-[50vh] 2xl:h-[60vh] px-8 sm:px-8 md:px-8 lg:px-20 xl:px-50 2xl:px-80 relative overflow-hidden">
         <div class="absolute right-0 left-0 bottom-[-300px] top-[65%] bg-[#D9D0E5] transform -skew-y-4 origin-bottom">
         </div>
         <div>
             <img src="/images/hero1.png"
-                class="absolute right-38 top-20 w-46 sm:right-38 sm:top-20 sm:w-46 sm:h-auto md:right-30 md:w-44 md:h-auto md:top-26 lg:right-60 lg:w-56 lg:h-auto lg:top-17 xl:right-60 xl:w-64 xl:h-auto xl:top-16 2xl:right-80 2xl:top-20 2xl:w-96 2xl:h-auto z-10"
+                class="absolute right-38 top-20 w-46 sm:right-38 sm:top-20 sm:w-46 sm:h-auto md:right-30 md:w-44 md:h-auto md:top-26 lg:right-60 lg:w-56 lg:h-auto lg:top-17 xl:right-60 xl:w-64 xl:h-auto xl:top-16 2xl:right-80 2xl:top-12 2xl:w-96 2xl:h-auto z-10"
                 alt="hero">
             <img src="/images/hero1.png"
-                class="absolute right-0 top-18 w-42 sm:right-0 sm:top-18 sm:w-42 sm:h-auto md:right-0 md:top-18 md:w-42 md:h-auto lg:right-18 lg:top-11 lg:w-52 lg:h-auto xl:right-12 xl:w-62 xl:h-auto 2xl:right-10 2xl:-top-20 2xl:w-86 2xl:h-auto"
+                class="absolute right-0 top-18 w-42 sm:right-0 sm:top-18 sm:w-42 sm:h-auto md:right-0 md:top-18 md:w-42 md:h-auto lg:right-18 lg:top-11 lg:w-52 lg:h-auto xl:right-12 xl:w-62 xl:h-auto 2xl:right-14 2xl:-top-10 2xl:w-86 2xl:h-auto"
                 alt="hero">
         </div>
         <div class="h-full flex flex-col justify-center gap-y-14 items-start relative z-10">
